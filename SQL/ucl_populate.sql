e97d0b3b62dcffa57974412147e77d8ee052e7ff
Rajdeep Bastakoti <r7b@outlook.com>
2022-01-12 02:28:49 +0100

    update sql

---
+++ SQL/ucl_populate.sql
@@ -0,0 +1,213 @@
+--Players
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('2', 'Kyle Walker', '31', 'DE', 'ENG', '5000000', '0', '2');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('3', 'Ruben Dias' , '24', 'DE' , 'POR', '6000000', '1', '1');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('17', 'Kevin De Bruyne', '30', 'MF', 'BEL', '20000000', '1', '3');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('7', 'Raheem Sterling', '26', 'FW', 'ENG', '15000000', '2', '1');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('10', 'Jack Grealish', '26', 'FW', 'ENG', '15000000', '2', '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('30', 'Lionel Messi', '32', 'FW', 'ARG', '24000000', '1', '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('2', 'Achraf Hakimi', '22', 'DE', 'MOR', '14000000', '0', '1');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('10', 'Neymar Dos Santos', '22', 'FW', 'BRA', '44000000', '2', '1');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('11', 'Angel Di Maria', '33', 'FW', 'ARG', '12000000', '1', '1');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('7', 'Kylian Mbappe', '22', 'FW', 'FRA', '20000000', '2', '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('1', 'Péter Gulácsi', '31', 'GK', 'HUN', '3120000', '0', '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('14', 'Tyler Adams', '22', 'MF', 'USA', '2130000', '0', '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('25', 'Dani Olmo', '23', 'MF', 'SPN', '3744000', '0', '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('10', 'Emil Frosberg', '29', 'FW', 'SWE', '3744000', '2', '1');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('16', 'Lukas Klostermann', '25', 'DF', 'GER', '4576000', '0', '1');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('20', 'Hans Vanaken', '28', 'MF', 'BELG', '2000000', '2', '1');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('77', 'Clinton Mata', '28', 'DF', 'ANG', '1000000', '0', '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('10', 'Noe Lang', '22', 'FW', 'NED', '1000000', '0' , '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('90', 'Mbaye Diagne', '27', 'FW', 'SEN', '2000000', '1', '1');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('9', 'Bryant Mayers', '23', 'MF', 'PR', '1000000', '1', '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('23', 'João Félix', '21', 'FW', 'POR', '9000000', '2', '3');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('8', 'Koke', '29', 'MF', 'ESP', '15000000', '1', '2');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('9', 'Luis Suárez', '34', 'FW', 'URU', '16000000', '4', '1');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('13', 'Jan Oblak', '28', 'GK', 'SVN', '19000000', '0', '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('12', 'Kieran Trippier', '31', 'DE', 'ENG', '6000000', '0', '1');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('4', 'Virgil Van Dijk', '30', 'DE', 'NED', '13000000', '2', '4');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('10', 'Sadio Mané', '29', 'FW', 'SEN', '7000000', '3', '1');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('9', 'Roberto Firmino', '30', 'FW', 'BRA', '11000000', '3', '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('11', 'Mohamed Salah', '29', 'FW', 'EGY', '12000000', '4', '1');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('66', 'Trent Alexander-Arnold', '23', 'DE', 'ENG', '10000000', '1', '2');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('99', 'Diogo Costa', '22', 'GK', 'POR', '10000000', '0', '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('19', 'Mbemba', '27', 'DE', 'COD', '16000000', '0', '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('3', 'PEPE', '38', 'DE', 'POR', '1000000', '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('17', 'Mehdi Taremi', '29', 'FW', 'IRN', '16000000', '1', '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('17', 'Jesús Corona', '28', 'MF', 'MEX', '30000000', '0', '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('16', 'Mike Maignan', '26', 'GK', 'FRA', '25000000', '0', '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('19', 'Theo Hernández', '24', 'DF', 'FRA', '50000000', '0', '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('17', 'Rafael Leão', '22', 'FW', 'POR', '25000000', '1', '1');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('9', 'Olivier Giroud', '35', 'FW', 'FRA', '4000000', '0', '0');
+
+INSERT INTO Player (s_num, p_name, age, pos, country, salary, goals, assists) 
+VALUES('11', 'Zlatan Ibrahimovic', '40', 'FW', 'SWE', '4000000', '0', '0');
+
+
+--Teams
+
+INSERT INTO Team(t_name, abbreviation, points, country, trophies, placement, hst)
+VALUES("Paris Saint-Germain", "PSG", "15", "FRA", "43", "1", "Parc des Princes");
+INSERT INTO Team(t_name, abbreviation, points, country, trophies, placement, hst)
+VALUES("Manchester City", "MNC", "13", "ENG", "28", "2");
+INSERT INTO Team(t_name, abbreviation, points, country, trophies, placement, hst)
+VALUES("RB Leipzig", "RBL", "10", "GER", "1", "3");
+INSERT INTO Team(t_name, abbreviation, points, country, trophies, placement, hst)
+VALUES("Club Brugge", "BRU", "7", "BEL", "44", "4");
+INSERT INTO Team(t_name, abbreviation, points, country, trophies, placement, hst)
+VALUES("Liverpool", "LIV", "16", "ENG", "63", "1");
+INSERT INTO Team(t_name, abbreviation, points, country, trophies, placement, hst)
+VALUES("Atletico de Madrid", "ATM", "14", "ESP", "31", "2"); 
+INSERT INTO Team(t_name, abbreviation, points, country, trophies, placement, hst)
+VALUES("AC Milan", "MIL", "12", "ITA", "51", "3");
+INSERT INTO Team(t_name, abbreviation, points, country, trophies, placement, hst)
+VALUES("FC Porto", "POR", "8", "POR", "77", "4");
+
+--Manager
+
+INSERT INTO Manager(m_name, c_trophies)
+VALUES("Mauricio Pochettino", "2");
+INSERT INTO Manager(m_name, c_trophies)
+VALUES("Pep Guardiola", "33");
+INSERT INTO Manager(m_name, c_trophies)
+VALUES("Jesse Marsch", "5");
+INSERT INTO Manager(m_name, c_trophies)
+VALUES("Philippe Clement", "7");
+INSERT INTO Manager(m_name, c_trophies)
+VALUES("Jürgen Klopp", "11");
+INSERT INTO Manager(m_name, c_trophies)
+VALUES("Diego Simeone", "9");
+INSERT INTO Manager(m_name, c_trophies)
+VALUES("Stefano Pioli", "0");
+INSERT INTO Manager(m_name, c_trophies)
+VALUES("Sérgio Conceição", "5");
+
+--Matches
+
+INSERT INTO Matches(m_date, result, p_earned, t1_name, t2_name)
+VALUES('2020-Oct-01', 'MNC(3-1)BRU', '3', 'MNC', 'BRU');
+
+INSERT INTO Matches(m_date, result, p_earned, t1_name, t2_name)
+VALUES('2020-Oct-02', 'PSG(2-4)ACM', '3', 'PSG', 'ACM');
+
+INSERT INTO Matches(m_date, result, p_earned, t1_name, t2_name)
+VALUES('2020-Oct-04', 'ATM(2-2)LIV', '1', 'ATM', 'LIV');
+
+INSERT INTO Matches(m_date, result, p_earned, t1_name, t2_name)
+VALUES('2020-Oct-03', 'RBG(0-0)POR', '1', 'RBG', 'POR');
+
+--Champions_League
+
+INSERT INTO Champions_League(l_year, mvp, winner, t1_name, t2_name)
+VALUES('2020', 'Kevin De Bruyne', 'MNC');
+
+--Groups
+
+INSERT INTO Groups(g_id, t_scorer, god)
+VALUES("A", "Kylian Mbappe", "0");
+
+INSERT INTO Groups(g_id, t_scorer, god)
+VALUES("B", "Luis Suares", "1");
+
+--Stadium
+
+INSERT INTO Stadium(stad_name, cap)
+VALUES("Etihad Stadium", "55017");
+
+INSERT INTO Stadium(stad_name, cap)
+VALUES("Parc des Princes", "49691");
+
+INSERT INTO Stadium(stad_name, cap)
+VALUES("Red Bull Arena", "47069");
+
+INSERT INTO Stadium(stad_name, cap)
+VALUES("Jan-Breydel-Stadion", "29062");
+
+INSERT INTO Stadium(stad_name, cap)
+VALUES("Wanda Metropolitano", "68456");
+
+INSERT INTO Stadium(stad_name, cap)
+VALUES("Anfield", "54074");
+
+INSERT INTO Stadium(stad_name, cap)
+VALUES("Estádio do Dragão", "50399");
+
+INSERT INTO Stadium(stad_name, cap)
+VALUES("Giuseppe Meazza", "75923");
